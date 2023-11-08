4b114a4c1d0c2f1792cd93d7d75ab84a8a2c74a2
elduroabreu <150207979+elduroabreu@users.noreply.github.com>
2023-11-07 21:16:38 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,54 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-acuario",
+      "name": "Acuario",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": -1.520474142115793,
+        "pitch": 0.26037443098650037,
+        "fov": 1.4057311061115183
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.223881865068078,
+          "pitch": 0.20932739578604043,
+          "rotation": 3.141592653589793,
+          "target": "0-acuario"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9135683541486799,
+          "pitch": 0.30769069958043715,
+          "title": "xd",
+          "text": "hola"
+        },
+        {
+          "yaw": 2.3397405394000277,
+          "pitch": 0.387995760835917,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
